2efc624ace4616ddb8f8e950dbf4b6d03ea130fb
Jakub Solarek <j.solarek@wp.pl>
2022-08-31 21:19:28 +0200

    Add first neutron and implement neutron functionality

--- A.I neural network/Program.cs
+++ A.I neural network/Program.cs
@@ -10,7 +10,16 @@ namespace A.I.neural.network
             int[] diameter = new int[9];
             int[] length = new int[9];
             int[] correctAnswer = new int[9];
-            for (int i = 0; i < 9; i++)
+            bool[] correct = new bool[9];
+            double[] result = new double[9];
+
+            Random r = new Random();
+            double[] weights = { -2, 0,1};
+            double mutaion = 6;
+
+            double learningRate = 1;            
+
+            for (int i = 0; i < 8; i++)
             {
                 Console.WriteLine("Enter the diameter");
                 diameter[i] = Convert.ToInt32(Console.ReadLine());
@@ -23,7 +32,28 @@ namespace A.I.neural.network
                     Console.WriteLine("{0} values were taken", i + 1);
                 }
             }
-            
+            Console.WriteLine();
+            Console.WriteLine();
+            for(int i = 0; i < 8; i++)
+            {
+                result[i] = (diameter[i] * weights[0]) + (length[i] * weights[1])+mutaion;
+                if (result[i] > 0 && correctAnswer[i] == 1)
+                {
+                    Console.WriteLine(result[i] + " Correct Ring");
+                }
+                if (result[i] > 0 && correctAnswer[i] == 2)
+                {
+                    Console.WriteLine(result[i] + " Incorrect Ring");
+                }
+                if (result[i] < 0 && correctAnswer[i] == 2)
+                {
+                    Console.WriteLine(result[i] + " Correct Pen");
+                }
+                if (result[i] < 0 && correctAnswer[i] == 1)
+                {
+                    Console.WriteLine(result[i] + " Incorrect Pen");
+                }
+            }
         }
     }
 }
