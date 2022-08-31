5d8081b0e1b4ccf7b7b342da92d9b67eb2d0dc8f
Jakub Solarek <j.solarek@wp.pl>
2022-08-31 19:44:45 +0200

    Add project files

---
+++ A.I neural network/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace A.I.neural.network
+{
+    class program
+    {
+        static void Main(string[] Args)
+        {
+            int[] diameter = new int[8];
+            int[] lenght = new int[8];
+            for(int i = 0; i <= 8; i++)
+            {
+                diameter[i] = int.TryParse(Console.ReadLine());
+            }
+        }
+    }
+}
