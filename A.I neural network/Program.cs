a44864a0e244f642443091378ffe74e0f792b02d
Jakub Solarek <j.solarek@wp.pl>
2022-08-31 19:53:55 +0200

    Variable download has been add

--- A.I neural network/Program.cs
+++ A.I neural network/Program.cs
@@ -7,12 +7,23 @@ namespace A.I.neural.network
     {
         static void Main(string[] Args)
         {
-            int[] diameter = new int[8];
-            int[] lenght = new int[8];
-            for(int i = 0; i <= 8; i++)
+            int[] diameter = new int[9];
+            int[] length = new int[9];
+            int[] correctAnswer = new int[9];
+            for (int i = 0; i < 9; i++)
             {
-                diameter[i] = int.TryParse(Console.ReadLine());
+                Console.WriteLine("Enter the diameter");
+                diameter[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the length");
+                length[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the correct answer (1-ring, 2 - pen)");
+                correctAnswer[i] = Convert.ToInt32(Console.ReadLine());
+                if (i <= 8)
+                {
+                    Console.WriteLine("{0} values were taken", i + 1);
+                }
             }
+            
         }
     }
 }
