edf718ee5011bc591fab076b205a3bfcf2b7ef37
Jakub Solarek <j.solarek@wp.pl>
2022-09-01 15:27:46 +0200

    Fixed neutron function

--- A.I neural network/Program.cs
+++ A.I neural network/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 
 namespace A.I.neural.network
@@ -12,9 +13,13 @@ namespace A.I.neural.network
             int[] correctAnswer = new int[9];
             bool[] correct = new bool[9];
             double[] result = new double[9];
+            int[] correctThings = new int[9];
 
             Random r = new Random();
-            double[] weights = { -2, 0,1};
+            double[] weights = { -2, 0.1};
+            double[] oldWeights = new double[2];
+            oldWeights[0] = weights[0];
+            oldWeights[1] = weights[1];
             double mutaion = 6;
 
             double learningRate = 1;            
@@ -31,29 +36,81 @@ namespace A.I.neural.network
                 {
                     Console.WriteLine("{0} values were taken", i + 1);
                 }
+                if (correctAnswer[i] == 1)
+                {
+                    correctThings[i] = 1;
+                }
+                else if (correctAnswer[i] == 2)
+                {
+                    correctThings[i] = -1;
+                }
             }
             Console.WriteLine();
             Console.WriteLine();
-            for(int i = 0; i < 8; i++)
+            for (int j = 0; j < 9; j++)
             {
-                result[i] = (diameter[i] * weights[0]) + (length[i] * weights[1])+mutaion;
-                if (result[i] > 0 && correctAnswer[i] == 1)
+                if (result[0] == 0)
                 {
-                    Console.WriteLine(result[i] + " Correct Ring");
+                    for (int i = 0; i < 8; i++)
+                    {
+                        result[i] = ((diameter[i] * weights[0]) + (length[i] * weights[1])) + mutaion;
+                        if (result[i] > 0 && correctAnswer[i] == 1)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Ring", correctAnswer[i]);
+                        }
+                        if (result[i] > 0 && correctAnswer[i] == 2)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Ring", correctAnswer[i]);
+                        }
+                        if (result[i] < 0 && correctAnswer[i] == 2)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Pen", correctAnswer[i]);
+                        }
+                        if (result[i] < 0 && correctAnswer[i] == 1)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Pen", correctAnswer[i]);
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
                 }
-                if (result[i] > 0 && correctAnswer[i] == 2)
+                else
                 {
-                    Console.WriteLine(result[i] + " Incorrect Ring");
-                }
-                if (result[i] < 0 && correctAnswer[i] == 2)
-                {
-                    Console.WriteLine(result[i] + " Correct Pen");
-                }
-                if (result[i] < 0 && correctAnswer[i] == 1)
-                {
-                    Console.WriteLine(result[i] + " Incorrect Pen");
+                    oldWeights[0] = weights[0];
+                    oldWeights[1] = weights[1];
+                    weights[0] = WeightChange(correctThings[j], diameter[j], learningRate);
+                    weights[0] = WeightChange(correctThings[j], length[j], learningRate);
+                    for (int i = 0; i < 8; i++)
+                    {
+                        result[i] = ((diameter[i] * weights[0]) + (length[i] * weights[1])) + mutaion;
+                        if (result[i] > 0 && correctAnswer[i] == 1)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Ring", correctAnswer[i]);
+                        }
+                        if (result[i] > 0 && correctAnswer[i] == 2)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Ring", correctAnswer[i]);
+                        }
+                        if (result[i] < 0 && correctAnswer[i] == 2)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Pen", correctAnswer[i]);
+                        }
+                        if (result[i] < 0 && correctAnswer[i] == 1)
+                        {
+                            Console.WriteLine(result[i] + " My aswer: {0}, computer answer: Pen", correctAnswer[i]);
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
                 }
             }
+        }
+        public static double WeightChange(int correctThing, double weigth, double learningRate)
+        {
+            double result=correctThing*weigth*learningRate;            
+            return result;
         }
     }
 }
